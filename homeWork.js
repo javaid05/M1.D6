5601dc5fbf27ff6bb6acabf7deb2e62ef2a39b1e
javaid05 <javaidmalik.khan@gmail.com>
2021-09-27 15:22:44 +0100

    first part done

--- homeWork.js
+++ homeWork.js
@@ -56,3 +56,73 @@ console.log(me)
 */
 delete me.skill // here we have delete the skills of array
 console.log(me)
+
+// JS Functions
+/* EXERCISE 1
+    Write a function called dice; it should randomize an integer number between 1 and 6.
+*/
+const dice = function (minValue , maxValue ){
+
+minValue = Math.ceil(minValue)
+maxValue = Math.floor(maxValue)
+let result= Math.floor( Math.random()*(maxValue-minValue)) // + min vale not added still show random number 
+console.log(result)
+}
+dice(1,8)  // here dice 
+
+/* EXERCISE 2
+    Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
+*/
+
+const whoIsBigger = function(num1,num2){
+ let result = Math.max(num1,num2)
+console.log(result) // will show the big number of two parameter of numbers
+}
+whoIsBigger(6,1)  
+
+/* EXERCISE 3
+    Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
+    Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
+*/
+const splitMe = function(str){
+
+ let splitString = str.split("")
+ //splitString.split("")
+ console.log(splitString)
+}
+splitMe("I love coding")
+
+/* EXERCISE 4
+    Write a function called deleteOne which receives a string and a boolean as parameters.
+    If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
+
+*/
+/* EXERCISE 5
+   Write a function called onlyLetters which receives a string as a parameter and returns it removing all the digits.
+   Ex.: onlyLetters("I have 4 dogs") => returns "I have  dogs"
+*/
+const onlyLetters = function(str){
+ //for(let letter in str)
+ let newstring = str.replace(/[0-9]/g,'');
+console.log(newstring)
+}
+onlyLetters("I 45have 65dogs")
+/* EXERCISE 6
+   Write a function called isThisAnEmail which receives a string as a parameter and returns true if the string is a valid email address.
+*/
+/*
+const isThisAnEmail = function( str1){
+ let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ // regular expression
+ if(mailformat.text(str1))
+
+ console.log("Valid email address:",str1)
+ else 
+ console.log("Incalid Email Address")
+}
+isThisAnEmail("mike@gmail.com")*/
+/* EXERCISE 23
+  Create a function called "isItPrime" that receives a number as a parameter and returns true if the given number is a prime number.
+*/
+const primeNumber = function(number1,nummber2){
+ 
+}
